c4fa45e25d21477804a5d500e97561f4a875c2f3
vlad99902 <legkovv@mail.ru>
2020-07-01 23:10:22 +0300

    1st day

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+      <input
+        v-model="message"
+        @keyup.esc = "clearMessage"
+        @keyup.enter = "alertMessage"
+        />
+      <button @click="clearMessage">Clear</button>
+        <h5 class="border-grey" v-show="message.length">{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+    data(){
+        return {
+            message: 'Hello, world'
+        }
+    },
+    methods: {
+        clearMessage (){
+            this.message = ''
+        },
+        /*hadnleKeyup (e){
+           // console.log(e)
+            if (e.keyCode == 27){
+                this.clearMessage()
+            }
+            else if (e.keyCode == 13){
+                this.alertMessage()
+            }
+        },*/
+        alertMessage(){
+            alert(this.message)
+        }
+    }
 }
 </script>
+
+<style>
+    .border-grey{
+        border: 1px solid grey;
+    }
+</style>
